c7a4bc65fc48e538d154b13136f6153cc1a9184d
prathamj22 <110449212+prathamj22@users.noreply.github.com>
2022-10-02 15:13:14 +0530

    Delete dhruuv directory

--- dhruuv/src/main/java/com/example/dhruuv/LoginPage.java
+++
@@ -1,4 +0,0 @@
-package com.example.dhruuv;
-
-public class LoginPage {
-}
--- dhruuv/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.dhruuv {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.dhruuv to javafx.fxml;
-    exports com.example.dhruuv;
-}
